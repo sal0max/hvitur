4e715a7bbaba434f128b020458c788dde514f481
Max <msal.coding@gmail.com>
2019-01-22 22:42:23 +0100

    Get rid of the cards for the blog posts.

--- partials/loop.php
+++ partials/loop.php
@@ -3,25 +3,21 @@
    ...
 -->
 <?php if (have_posts()): ?>
-   <div class="card-columns">
 
    <?php while (have_posts()) : the_post(); ?>
-      <!-- card -->
-      <article class="card" id="post-<?php the_ID(); ?>">
+      <article class="mb-4" id="post-<?php the_ID(); ?>">
 
-         <!-- thumbnail -->
-         <?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-         <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-            <?php the_post_thumbnail('medium_large', array('class' => 'card-img-top img-fluid')); ?>
-         </a>
-         <?php endif; ?>
-         <!-- /thumbnail -->
+         <div>
+            <!-- date -->
+            <span class="small font-weight-light text-muted">
+               <?php echo get_the_date(); ?>
+            </span>
+            <!-- /date -->
 
-         <div class="card-body">
             <!-- title -->
-            <h3 class="card-title text-left">
-               <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-            </h3>
+            <div class="text-left">
+               <a class="h3" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+            </div>
             <!-- /title -->
 
             <!-- excerpt -->
@@ -29,19 +25,9 @@
             <!-- /excerpt -->
          </div>
 
-         <div class="card-footer text-right" style="background-color: #fff">
-            <small>
-               <!-- date -->
-               <?php echo get_the_date(); ?>
-               <!-- /date -->
-            </small>
-         </div>
-
       </article>
-      <!-- /card -->
    <?php endwhile; ?>
 
-   </div>
 <?php else: ?>
    <!-- error message -->
    <article>
