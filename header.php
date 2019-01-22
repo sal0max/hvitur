0587ddecc2e2cf82e82ad51a1e08e8f21ad0da1a
Max <msal.coding@gmail.com>
2019-01-22 11:22:41 +0100

    Fix W3C validation error: header/meta

--- header.php
+++ header.php
@@ -3,8 +3,7 @@
 <head>
    <title><?php wp_title(''); if(wp_title('', false)) { echo ' | '; } bloginfo('name'); ?></title>
    <meta charset="<?php bloginfo('charset'); ?>">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <meta name="description" content="<?php bloginfo('description'); ?>">
    <?php wp_head(); ?>
    <!-- rss -->
