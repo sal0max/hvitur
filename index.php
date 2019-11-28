1fa768127073658bcad107ae989219a69aa17773
Max <msal.coding@gmail.com>
2019-11-29 00:39:58 +0100

    some new translations

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ get_template_part('partials/container'); ?>
    <main>
       <!-- category or tag: set title -->
       <?php if(is_category() || is_tag()) : ?>
-         <h1 class="page-title display-6"><?php printf(__('Everything about %s', 'hvitur'), '<strong>' . wp_title('', false) . '</strong>'); ?></h1>
+         <h1 class="page-title display-6"><?php printf('<strong>' . wp_title('', false) . '</strong>'); ?></h1>
          <hr>
       <?php endif; ?>
       <!-- /category or tag: set title -->
